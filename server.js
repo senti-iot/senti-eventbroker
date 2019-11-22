22cdfbb834a8c90ff165f2c78876dd47e8b9f009
Andrei Tudor <andrei.tudor27@gmail.com>
2019-11-22 16:23:44 +0100

    Push Messages

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env nodejs
+// eslint-disable-next-line no-unused-vars
 const dotenv = require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const app = express()
+const webpush = require('web-push');
 
+//eslint-disable-next-line
+webpush.setVapidDetails('mailto:at@webhouse.dk', process.env.PUBLIC_VAPID_KEY, process.env.PRIVATE_VAPID_KEY)
 // API endpoint imports
 
+// eslint-disable-next-line no-undef
 const port = process.env.NODE_PORT || 3007
 
 app.use(helmet())
@@ -15,6 +20,17 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(cors())
 
+app.post('/subscribe', (req, res) => {
+	const subscription = req.body;
+	res.status(201).json({});
+	const payload = JSON.stringify({ title: 'test' });
+
+	console.log(subscription);
+
+	webpush.sendNotification(subscription, payload).catch(error => {
+		console.error(error.stack);
+	});
+});
 
 //---Start the express server---------------------------------------------------
 
