a693feb17ad334cc83d42496e7c51e69c109a77b
Mikkel Broberg <mb@webhouse.dk>
2021-12-17 12:05:23 +0100

    event v2 beta 1

--- mqtt/secureEventMqttHandler.js
+++ mqtt/secureEventMqttHandler.js
@@ -1,24 +1,46 @@
 const secureMqttHandler = require('senti-apicore').secureMqttHandler
 const eventService = require('../lib/event/eventService')
+const eventServiceV2 = require('../lib/event/eventServiceV2')
 
 class secureEventMqttHandler extends secureMqttHandler {
 	eventService = null
 
 	init() {
 		this.eventService = new eventService()
-		this.topics = ['v1/event/#']
+		this.eventServiceV2 = new eventServiceV2()
+		this.topics = ['v1/event/#', 'v2/event/#']
 		this.mqttClient.on('message', (topic, message) => {
-			// console.log('mqtt', topic, JSON.parse(message.toString()))
-			let arr = topic.split('/')
-			let data = JSON.parse(message.toString())
-			switch (arr[2]) {
-				case 'data':
-					this.eventService.findRulesAndExecute(data, parseInt(arr[3]), parseInt(arr[4]), parseInt(arr[5]))
-					break;
+			let topics = topic.split('/')
+			try {
+				let data = JSON.parse(message.toString())
+				switch (topics[0]) {
+					case 'v1':
+						// this.v1Message(topics, data)
+						break;
+					case 'v2':
+						this.v2Message(topics, data)
+						break;
+				}
+			}
+			catch(error) {
+				console.log('event json parse error', error, message.toString())
 			}
 		})
 	}
-
-
+	v1Message(topics, message) {
+		switch (topics[2]) {
+			case 'data':
+				this.eventService.findRulesAndExecute(message, parseInt(topics[3]), parseInt(topics[4]), parseInt(topics[5]))
+				break;
+		}
+	}
+	v2Message(topics, message) {
+		switch (topics[2]) {
+			case 'data':
+				// console.log(message)
+				this.eventServiceV2.findRulesAndExecute(message, topics[3], topics[4], topics[5])
+				break;
+		}
+	}
 }
 module.exports = secureEventMqttHandler
